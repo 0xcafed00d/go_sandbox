de3d686193b52f10e9852d53463ceb98362e2288
SimulatedSimian <neomech@gmail.com>
2014-06-27 00:33:20 +0100

    deleted uneeded code

--- elitecore/c/EliteGoatSoup.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include "EliteRNG.h"
-#include "ElitePlanetSeed.h"
-
-std::string generateGoatSoupString (EliteRNG::seed_t seed, const std::string& planetName, const char* digrams);
-
-std::string generateInhabitantDescription (const PlanetSeed_t& seed);
