98af6a8d2e0f3041b575e17fdcff5a69b69ff367
Lee Witek <neomech@gmail.com>
2016-01-12 09:49:48 -0500

    adding timeouts to connect

--- sandbox/socket/socket.go
+++ sandbox/socket/socket.go
@@ -23,7 +23,7 @@ func getAddress(host string) ([4]byte, error) {
 	return addr, nil
 }
 
-func connect(host string, port, ttl, timeout int) error {
+func connect(host string, port, ttl, timeoutMS int) error {
 	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
 	if err != nil {
 		return err
@@ -50,9 +50,36 @@ func connect(host string, port, ttl, timeout int) error {
 		return err
 	}
 
+	fdset := &syscall.FdSet{}
+	timeout := &syscall.Timeval{}
+
+	FD_ZERO(fdset)
+	FD_SET(fdset, sock)
+	_, err = syscall.Select(sock+1, nil, nil, fdset, timeout)
+	if err != nil {
+		return err
+	}
+
+	if FD_ISSET(fdset, sock) {
+	}
+
 	return nil
 }
 
 func main() {
 	fmt.Println(connect("www.ebay.com", 80, 8, 100))
+}
+
+func FD_SET(p *syscall.FdSet, i int) {
+	p.Bits[i/64] |= 1 << uint(i) % 64
+}
+
+func FD_ISSET(p *syscall.FdSet, i int) bool {
+	return (p.Bits[i/64] & (1 << uint(i) % 64)) != 0
+}
+
+func FD_ZERO(p *syscall.FdSet) {
+	for i := range p.Bits {
+		p.Bits[i] = 0
+	}
 }
