26e44a6ac6671221aa3dae7eff74b47f70b189b3
Lee Witek <neomech@gmail.com>
2017-07-29 01:42:47 +0100

    adding helper funtions

--- gopherjstest/main.go
+++ gopherjstest/main.go
@@ -8,25 +8,37 @@ import (
 	"honnef.co/go/js/dom"
 )
 
+type Canvas struct {
+	ctx     *dom.CanvasRenderingContext2D
+	Animate func(t time.Duration)
+}
+
+func MakeCanvas(id string) *Canvas {
+	c := Canvas{}
+
+	doc := dom.GetWindow().Document()
+	canvas := doc.GetElementByID(id).(*dom.HTMLCanvasElement)
+	c.ctx = canvas.GetContext2d()
+	return &c
+}
+
 func main() {
 
 	js.Global.Call("addEventListener", "load", func() {
 		doc := dom.GetWindow().Document()
-		canvas := doc.GetElementByID("canvas").(*dom.HTMLCanvasElement)
-		ctx := canvas.GetContext2d()
 		img := doc.GetElementByID("img_elephant").(*dom.HTMLImageElement)
-
+		canvas := MakeCanvas("canvas")
 		println(img)
 		var x = 1
 		var aniFrm func(t time.Duration)
 		aniFrm = func(t time.Duration) {
-			ctx.FillStyle = "black"
-			ctx.FillRect(0, 0, 640, 480)
-			ctx.FillStyle = "red"
+			canvas.ctx.FillStyle = "black"
+			canvas.ctx.FillRect(0, 0, 640, 480)
+			canvas.ctx.FillStyle = "red"
 			s := fmt.Sprint(t)
-			ctx.FillText(s, x, 100, 300)
+			canvas.ctx.FillText(s, x, 100, 300)
 
-			ctx.Call("drawImage", img.Object, x, 100, 100, 100)
+			canvas.ctx.Call("drawImage", img.Object, x, 100, 100, 100)
 
 			x++
 			if x > 640 {
